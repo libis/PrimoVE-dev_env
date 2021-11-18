4c5bfd815a85322698987ca4fdca5c104ed7270e
Tom Vanmechelen <tom.vanmechelen@libis.be>
2021-11-18 16:15:25 +0100

    language selection

---
+++ src/components/languageSelection/index.js
@@ -0,0 +1,29 @@
+
+class LanguageSelectionController {
+    constructor($scope) {
+        let self = this;
+        let scope = $scope;
+        var onlyshow_fr_FR_in_vid = ['ACV', 'BPB', 'FARO', 'FODFIN', 'KBC', 'RBINS', 'RMCA', 'KMKG', 'LIBAR', 'NBB', 'OFO', 'VDIC'];
+        let parentEl = angular.element(document.querySelector('primo-explore'));
+        if (! onlyshow_fr_FR_in_vid.includes( window.appConfig.vid  ) ) {
+            parentEl.append( "<style>prm-language-selection md-list-item[value='fr']{display:none!important;}</style>" );
+        }
+
+    }
+}
+
+LanguageSelectionController.$inject = ['$scope']
+
+export let LanguageSelectionConfig = {
+    name: 'custom-language-selection',  
+    enabled: true,
+    appendTo: 'prm-main-menu-after',
+    enableInView: '.*',
+    config: {  
+      bindings: {
+        parentCtrl: '<'
+      },
+      controller: LanguageSelectionController,
+      template: ''
+    }
+  }
