3c2fade61321b81d91c2a352d4e344ed6e589982
Tom Vanmechelen <tom.vanmechelen@libis.be>
2021-11-18 15:57:46 +0100

    Search Also icons and Unicat

--- src/components/searchAlsoBody/index.js
+++ src/components/searchAlsoBody/index.js
@@ -87,13 +87,26 @@ class SearchAlsoBodyController {
         "img": "https://scholar.google.com/favicon.ico",
         "tooltip": "nui.customizing.idslu.search_also.tooltip.google_scolar",
         mapping: function mapping(search) {
-          return self.searchTerms;
+          var terms = search.split(",");
+          return terms[2] || "";
         }
-      }, {
+      },
+      {
+        "view": '^32KUL_KATHO:VIVES|^32KUL_HUB:ODISEE|^FARO|^32KUL_KHK:TMOREK|^32KUL_KHM:TMOREMA|^32KUL_KHLL:UCLL|^32KUL_KUL:KULeuven',
+        "name": "UniCat",
+        "url": "http://kuleuven.e-bronnen.be/login?url=https://unicat.be/uniCat?func=search&uiLanguage=en&query=",
+        "img": "/discovery/custom/32KUL_LIBIS_NETWORK-CENTRAL_PACKAGE/img/favicon_unicat.ico",
+        "tooltip": "nui.customizing.idslu.search_also.tooltip.unicat",
+        mapping: function mapping(search) {
+          var terms = search.split(",");
+          return terms[2] || "";
+        }
+      },
+       {
         "view": '^32KUL_KATHO:VIVES|^32KUL_HUB:ODISEE|^FARO|^32KUL_KHK:TMOREK|^32KUL_KHM:TMOREMA|^32KUL_KHLL:UCLL|^32KUL_KUL:KULeuven',
         "name": "Worldcat",
         "url": "https://www.worldcat.org/search?q=",
-        "img": "https://slsp-network.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU15/img/favicon_worldcat.png",
+        "img": "/discovery/custom/32KUL_LIBIS_NETWORK-CENTRAL_PACKAGE/img/favicon_worldcat.ico",
         "tooltip": "nui.customizing.idslu.search_also.tooltip.worldcat",
         mapping: function mapping(search) {
           var type_mappings = {
@@ -108,7 +121,7 @@ class SearchAlsoBodyController {
         "view": '^32KUL_KATHO:VIVES|^32KUL_HUB:ODISEE|^FARO|^32KUL_KHK:TMOREK|^32KUL_KHM:TMOREMA|^32KUL_KHLL:UCLL',
         "name": "Bibliotheek.be",
         "url": "https://www.bibliotheek.be/catalogus?q=",
-        "img": "https://slsp-network.primo.exlibrisgroup.com/discovery/custom/41SLSP_RZS-VU15/img/bib_be.png",
+        "img": "/discovery/custom/32KUL_LIBIS_NETWORK-CENTRAL_PACKAGE/img/bib_be.png",
         "tooltip": "nui.customizing.idslu.search_also.tooltip.deBib",
         mapping: function mapping(search) {
           var terms = search.split(",");
