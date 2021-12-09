bb39b84e09d1b4cbb4390be4458c68eaa647baf7
Mehmet Celik <mehmetc@celik.be>
2021-12-09 14:52:23 +0100

    merging facets and info

--- src/factories/blendedSearch/blended_search.js
+++ src/factories/blendedSearch/blended_search.js
@@ -1,11 +1,8 @@
+const { merge } = require('angular');
 const syncFetch = require('sync-fetch');
 
 window.blendedSearch = {
     active: false,
-    result: {},
-    params: {},
-    get limit() { return Math.floor(blendedSearch.params.limit / 2) },
-    get offset() { return Math.floor(blendedSearch.params.offset / 2) },
     activate: () => {
         let windowParams = window.location.search.slice(1).split('&').map(m => m.split('=')).reduce((map, obj) => { map[obj[0]] = obj[1]; return map }, {})
         if (windowParams.hasOwnProperty('blend')) {
@@ -16,68 +13,140 @@ window.blendedSearch = {
             }
         }
     },
-    search: (url, headers, params) => {
-        blendedSearch.result = {};
-        // clone and prepare parameters
+    init: (url, headers, params, data) => {
+        blendedSearch.set1.url = url;
+        blendedSearch.set1.headers = headers;
+        blendedSearch.set1.params = JSON.parse(JSON.stringify(params));
+        blendedSearch.set1.data = data;
+
+        blendedSearch.set2.url = url;
+        blendedSearch.set2.headers = headers;
+        blendedSearch.set2.data = {};
+
         let cloned_params = JSON.parse(JSON.stringify(params));
         cloned_params['scope'] = 'lirias_profile';
         cloned_params['pcAvailability'] = true;
-
-        let esURL = new URL(`${window.location.origin}${url}`);
-        Object.keys(cloned_params).forEach(key => {
-            if (cloned_params[key] != undefined) {
-                esURL.searchParams.append(key, cloned_params[key])
+        blendedSearch.set2.params = cloned_params;
+    },
+    set1: {
+        url: '',
+        headers: {},
+        params: {},
+        data: {},
+        get score() {
+            let s = 1;
+            try {
+                s = blendedSearch.set1.data.docs[0].pnx.control.score;
+            } catch (error) {
+                s = 1;
             }
-        });
 
-        //fetch result
-        result = syncFetch(esURL, { method: 'GET', headers: headers }).json();
-        blendedSearch.result = result;
-        
-        return result;
+            return s;
+        },
+        get limit() {
+            let data = blendedSearch.limitOffset()
+            return data[0];
+        },
+        get offset() {
+            let data = blendedSearch.limitOffset()
+            return data[1];
+        }
+    },
+    set2: {
+        url: '',
+        headers: {},
+        params: {},
+        data: {},
+        get score() {
+            let s = 1;
+            try {
+                s = blendedSearch.set2.data.docs[0].rank;
+            } catch (error) {
+                s = 1;
+            }
+
+            return s;
+        },
+        search: () => {
+            blendedSearch.set2.data = {};
+
+            let esURL = new URL(`${window.location.origin}${blendedSearch.set2.url}`);
+            Object.keys(blendedSearch.set2.params).forEach(key => {
+                if (blendedSearch.set2.params[key] != undefined) {
+                    esURL.searchParams.append(key, blendedSearch.set2.params[key])
+                }
+            });
+
+            //fetch result
+            result = syncFetch(esURL, { method: 'GET', headers: blendedSearch.set2.headers }).json();
+            blendedSearch.set2.data = result;
+
+            return result;
+        },
+        get limit() {
+            let data = blendedSearch.limitOffset()
+            return data[2];
+        },
+        get offset() {
+            let data = blendedSearch.limitOffset()
+            return data[3];
+        }
+    },
+    limitOffset() { 
+        let t1 = 0;
+        let l1 = Math.ceil(this.set1.params.limit / 2);
+        let o1 = Math.ceil(this.set1.params.offset / 2);
+
+        let t2 = this.set2.data.info.total;
+        let l2 = l1;
+        let o2 = o1;
+
+        if ((t2 - o2) < l2) {
+            l2 = t2 - o2
+            l2 = l2 < 0 ? 0 : l2 //compensate for o2 > t2
+        }
+
+        if (l2 < l1) {
+            l1 = l1 + (l1 - l2)
+        }
+
+        return [l1, o1, l2, o2];
+    },
+    get offset() { 
+        return Math.floor(blendedSearch.set1.params.offset / 2) 
     }
 };
 
 angular.module('blendedSearch', ['ng']).run(() => {
 
     pubSub.subscribe('before-pnxBaseURL', (url, headers, params, data) => {
-        blendedSearch.params = params;
+        blendedSearch.init(url, headers, params);
+        blendedSearch.set2.search();
 
-        data.params.limit = blendedSearch.limit;
-        data.params.offset = blendedSearch.offset;
+        data.params.limit = blendedSearch.set1.limit;
+        data.params.offset = blendedSearch.set1.offset;
         return data;
     })
 
     // federated search and merge result set
     pubSub.subscribe('after-pnxBaseURL', (url, headers, params, data) => {
-        if (params['scope'] == 'lirias_profile') {
-            console.log('==============>', JSON.stringify(params))
-        } else {
-            let result = blendedSearch.search(url, headers, params);
+        if (params['scope'] != 'lirias_profile') {
+            let result = blendedSearch.set2.data;
+            blendedSearch.set1.data = JSON.parse(JSON.stringify(data));
             console.log('BLENDING ResultSet1:', JSON.stringify(data.info));
             console.log('BLENDING ResultSet2:', JSON.stringify(result.info));
 
             //process result 
-            let newLimitSet1 = data.docs.length < params.limit ? data.docs.length : params.limit
-            let newLimitSet2 = result.docs.length < params.limit ? result.docs.length : params.limit;
+            let newLimitSet1 = blendedSearch.set1.limit;
+            let newLimitSet2 = blendedSearch.set2.limit;
 
+            // DOCS
             let docs = [];
             docs = data.docs.slice(0, newLimitSet1);
             docs = docs.concat(result.docs.slice(0, newLimitSet2));
 
-            let rankSet1 = 1;
-            try {
-                rankSet1 = data.docs[0].pnx.control.score;
-            } catch (error) {
-                rankSet1 = 0;
-            }
-
-            let rankSet2 = 1;
-            try {
-                rankSet2 = result.docs[0].rank;
-            } catch (error) {
-                rankSet2 = 1;
-            }
+            let rankSet1 = blendedSearch.set1.score;
+            let rankSet2 = blendedSearch.set2.score;
 
             docs = docs.map(m => {
                 if (m.hasOwnProperty('rank')) {
@@ -89,6 +158,37 @@ angular.module('blendedSearch', ['ng']).run(() => {
             docs = docs.sort((a, b) => b.pnx.control.score - a.pnx.control.score);
 
             data['docs'] = docs;
+
+
+            // FACETS
+            let facets = data.facets;
+            let facetMap = result.facets.reduce((map, obj) => { map[obj['name']] = obj; return map }, {});
+            facets.forEach((v, i, a) => {
+                //merge into facet
+                if (facetMap.hasOwnProperty(v['name'])) {                                        
+                    let mergedFacet = v['values'].concat(facetMap[v['name']]['values']).reduce((map, obj) => { 
+                        if (map.hasOwnProperty(obj['value'])) {
+                            map[obj['value']] += obj['count'];                            
+                        } else {
+                            map[obj['value']] = parseInt(obj['count']);                                                       
+                        }
+                        return map;
+                    }, {});
+                    
+                    let p = [];
+                    //transform into object
+                    Object.keys(mergedFacet).forEach(k => p.push({'value': k, 'count': mergedFacet[k]}))
+
+                    v['values'] = p;
+                }
+            });
+                        
+            data['facets'] = facets;
+
+            data['info']['total'] += result['info']['total'];
+
+            data
+
         }
         return data;
     });
@@ -103,3 +203,12 @@ angular.module('blendedSearch', ['ng']).run(() => {
     //     return data;
     // });    
 })
+
+
+// t1 = blendedSearch.set1.data.info.total
+// l1 = blendedSearch.set1.params.limit
+// o1 = blendedSearch.set1.params.offset
+
+// t2 = blendedSearch.set2.data.info.total
+// l2 = blendedSearch.set2.params.limit
+// o2 = blendedSearch.set2.params.offset
