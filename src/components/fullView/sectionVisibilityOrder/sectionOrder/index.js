5846963fcd8e2354b556223366dda5f87f21f3d8
Tom Vanmechelen <tom.vanmechelen@libis.be>
2022-03-24 16:19:39 +0100

    optimization section order

--- src/components/fullView/sectionVisibilityOrder/sectionOrder/index.js
+++ src/components/fullView/sectionVisibilityOrder/sectionOrder/index.js
@@ -35,14 +35,52 @@ class SectionOrderController {
         self.$element = $element;
         self.$translate = $translate;
 
+        window.fullViewServiceOrder = [];
+        
+        self.appendToElement = document.getElementsByTagName("primo-explore")[0];
+
+        self.servicesOrder = self.parentCtrl.parentCtrl.fullViewService.configurationUtil.getBriefResultConfiguration().tabsorder.items.split(',');
+        // brief-section always on top 
+        self.servicesOrder.unshift('brief')
+        
+        // citationTrails-section aways after brief
+        self.servicesOrder.splice(self.servicesOrder.indexOf('brief'), 1, 'brief', 'citationTrails')
+        
+        // action_list-section (send-to) aways before details
+        self.servicesOrder.splice(self.servicesOrder.indexOf('details'), 1, 'action_list', 'details')
+
+        // action_list-section (send-to) aways after links-section
+        // self.servicesOrder.splice(self.servicesOrder.indexOf('links'), 1, 'links', 'action_list')
+        let services_keys = Object.keys(self.parentCtrl.parentCtrl.fullViewService.servicesDirectives);
+        //let difference = services_keys.filter(s => !self.servicesOrder.includes(s));
+        self.servicesOrder = self.servicesOrder.concat(services_keys.filter(s => !self.servicesOrder.includes(s)));
+
+        if (self.parentCtrl.parentCtrl.$stateParams.vid === "32KUL_KUL:Lirias") {
+            self.servicesOrder = ["brief", "getit_link1", "details", "links", "altmetrics", "action_list", "tags", "citationTrails"];
+        }
         let translatorWatcher = $scope.$watch(() => {
             return self.$translate.isReady()
         }, (n, o) => {
             if (n == true) {
+
+                services_keys.forEach(function (service_key) {
+                    var service = self.parentCtrl.parentCtrl.fullViewService.servicesDirectives[service_key];
+                    var orderId = service["scrollId"]
+                    if (orderId){
+                        var order = self.servicesOrder.indexOf(orderId);
+                        if (order < 0) { 
+                            order = 50
+                        }
+                        self.addStyle(order,service)
+                    }
+                });
+
                 let servicesWatcher = $scope.$watch(() => {
                     let servicesLoaded = self.parentCtrl.parentCtrl.fullViewService.servicesArray !== undefined;
-                    let calculatePrimaViewItDone = self.parentCtrl.parentCtrl.fullViewService.calculatePrimaViewItDone()
-                    return (servicesLoaded && calculatePrimaViewItDone);
+                    let calculatePrimaViewItDone = self.parentCtrl.parentCtrl.fullViewService.calculatePrimaViewItDone();
+                    let calculatePcDeliveryDone = self.parentCtrl.parentCtrl.fullViewService.calculatePcDeliveryDone;
+                    let calculateSvcIdDone = self.parentCtrl.parentCtrl.fullViewService.calculateSvcIdDone;
+                    return (servicesLoaded && calculatePrimaViewItDone && calculatePcDeliveryDone && calculateSvcIdDone);
                 }, (n, o) => {
                     if (n == true) {
                         self.addStyles(self)
@@ -53,188 +91,53 @@ class SectionOrderController {
             }
         }, false);
 
-        /*
-            if (serviceName === 'display') {
-                this.viewOnlineScrollId = 'getit_link1_' + scrollIdIndex;
-            }
-            array.push(
-                {
-                    scrollId: 'getit_link1_' + scrollIdIndex,
-                    serviceName: serviceName,
-                    title: 'nui.getit.' + value.links[0].getItTabText,
-                    linkElement: value
-                }
-            );
-        
-        
-              'brief': {
-                        scrollId: 'brief',
-                        serviceName: 'brief',
-                        title: 'nui.brief.results.tabs.brief'
-                    },
-                    'citations': {
-                        scrollId: 'citationTrails',
-                        serviceName: 'citationTrails',
-                        title: 'citation_trail.link.citations',
-                        conditional: (item: PrimoResponse.IDoc) => {
-                            return (this.recordUtil.hasCitationTrails(item) && this.citationTrailsFullviewLinkService.shouldDisplayCitationTrial()) || this.citationTrailsFullviewLinkService.shouldDisplayTimesCited();
-                        }
-                    },
-                    'action_list': {
-                        scrollId: 'action_list',
-                        serviceName: 'action_list',
-                        title: 'nui.brief.results.tabs.send_to'
-                    },
-                    'esploroUsage': {
-                        scrollId: 'esploroUsage',
-                        serviceName: 'esploroUsage',
-                        title: 'nui.brief.results.tabs.esploroUsage'
-                    },
-                    'getit_link2': {
-                        serviceName: 'getit_link2', title: 'Get it 2'
-                    },
-                    'getit_link1': {
-                        scrollId: 'getit_link1',
-                        serviceName: 'getit_link1',
-                        title: 'brief.results.tabs.locations'
-                    },
-                    'tags': {
-                        scrollId: 'tags',
-                        serviceName: 'tags',
-                        title: 'brief.results.tabs.tags'
-                    },
-                    'rapidoOffer': {
-                        scrollId: 'rapidoOffer',
-                        serviceName: 'rapidoOffer',
-                        title: 'brief.results.tabs.Get_it_from_other_locations'
-                    },
-                    'quickAccess': {
-                        scrollId: 'quickAccess',
-                        serviceName: 'quickaccess',
-                        title: 'nui.brief.results.tabs.quickAccess'
-                    },
-                    'details': {
-                        scrollId: 'details',
-                        serviceName: 'details',
-                        title: 'brief.results.tabs.details'
-                    },
-                    'links': {
-                        scrollId: 'links',
-                        serviceName: 'links',
-                        title: 'nui.brief.results.tabs.links'
-                    },
-                    'browseshelf': {
-                        scrollId: 'virtualBrowse',
-                        serviceName: 'virtualBrowse',
-                        title: 'brief.results.tabs.browseshelf'
-                    },
-                    'moreLikeThisCourse': {
-                        scrollId: 'moreFromTheSameCourse',
-                        serviceName: 'moreLikeThisCourse',
-                        title: 'nui.brief.results.tabs.courseTab',
-                        tabTitle: 'nui.brief.results.tabs.moreCourse',
-                        conditional: (item: PrimoResponse.IDoc) => {
-                            return getAttributesMap().moreLikeCourse ? getAttributesMap().moreLikeCourse : false;
-                        }
-                    },
-                    'moreLikeThisCollection': {
-                        scrollId: 'moreFromTheSameCollection',
-                        serviceName: 'moreLikeThisCollection',
-                        title: 'nui.brief.results.tabs.collectionTab',
-                        tabTitle: 'nui.brief.results.tabs.moreCollection',
-                        conditional: (item: PrimoResponse.IDoc) => {
-                            return getAttributesMap().moreLikeCollection ? getAttributesMap().moreLikeCollection : false;
-                        }
-                    },
-                    'searchWithinJournal': {
-                        scrollId: 'searchWithinJournal',
-                        serviceName: 'searchWithinJournal',
-                        title: 'brief.results.tabs.searchWithinJournal'
-                    }, 
-                    'timesCited': {
-                        scrollId: 'citationTrails',
-                        serviceName: 'timesCited',
-                        title: 'citation_trail.link.citations',
-                        conditional: (item: PrimoResponse.IDoc) => {
-                            return this.timesCitedService.shouldDisplayTimesCited;
-                        }
-                    },
-                    'mayAlsoBeFoundAt': {
-                        scrollId: 'mayAlsoBeFoundAt',
-                        serviceName: 'mayAlsoBeFoundAt',
-                        title: 'nui.brief.results.tabs.mayAlsoBeHeldByTab',
-                        tabTitle: 'nui.brief.results.tabs.mayAlsoBeFoundAt',
-                        conditional: (item: PrimoResponse.IDoc) => {
-                            return getAttributesMap().mayAlsoBeHeldByEnabled ? getAttributesMap().mayAlsoBeHeldByEnabled : false;
-                        }
-                    }
-        //                                        'reviewsandtags'  : {'<prm-customization-iframe url-template-id="fullViewUrl"></prm-customization-iframe>'},
-        
-        //                    'recommendations': {},
-        //                    'citations': {},
-        //                    'onlinereviews': {},
-        //                    'browseshelf': {}
-        
-        */
-
-
     }
 
     addStyles() {
         var self = this
 
-        var appendToElement = document.getElementsByTagName("primo-explore")[0];
-
-        let servicesOrder = self.parentCtrl.parentCtrl.fullViewService.configurationUtil.getBriefResultConfiguration().tabsorder.items.split(',');
-        // brief-section always on top 
-        servicesOrder.unshift('brief')
-        // citationTrails-section aways after brief
-        servicesOrder.splice(servicesOrder.indexOf('brief'), 1, 'brief', 'citationTrails')
-        // action_list-section (send-to) aways after links-section
-        servicesOrder.splice(servicesOrder.indexOf('links'), 1, 'links', 'action_list')
-
-        let services_keys = Object.keys(self.parentCtrl.parentCtrl.fullViewService.servicesDirectives);
-        //let difference = services_keys.filter(s => !servicesOrder.includes(s));
-        servicesOrder = servicesOrder.concat(services_keys.filter(s => !servicesOrder.includes(s)));
-
-        // 
-        if (self.parentCtrl.parentCtrl.$stateParams.vid === "32KUL_KUL:Lirias") {
-            servicesOrder = ["brief", "getit_link1", "details", "links", "altmetrics", "action_list", "tags", "citationTrails"];
-        }
-
-        // console.log(servicesOrder)
+        console.log( self.parentCtrl.parentCtrl.fullViewService.servicesArray )
         self.parentCtrl.parentCtrl.fullViewService.servicesArray.forEach(function (service) {
             var orderId = service["scrollId"].replace(/getit_link1.*/, 'getit_link1').replace(/getit_link2.*/, 'getit_link2')
             var styleId = 'style_' + service["scrollId"]
-            var order = servicesOrder.indexOf(orderId);
+            var order = self.servicesOrder.indexOf(orderId);
 
-            /*
             console.log (styleId)
             console.log (service)
-            */
-            if (appendToElement.querySelector('style#' + styleId)) {
-                //next 
-                return;
-            }
 
             if (order < 0) {
-                console.warn("Service not found in servicesOrder");
-                console.log(service);
+                console.warn("Service ["+ service.serviceName +"] not found in servicesOrder");
                 order = 50;
             }
 
-            order++;
-
-            var s = document.createElement("style");
-            s.setAttribute("id", styleId);
-            s.innerHTML = ""
-            s.innerHTML += "div#services-index button[aria-label=\"" + self.$translate.instant(service["title"]) + "\"] { order: " + order + " !important;}";
-            s.innerHTML += "div.full-view-section#" + service["scrollId"] + " { order: " + order + " !important;}";
-
-            appendToElement.appendChild(s);
+            self.addStyle(order,service)
 
         });
 
+    }
+
+    addStyle(order,service) {
+        var self = this
+
+        var styleId = 'style_' + service["scrollId"]
+        if ( window.fullViewServiceOrder.includes(styleId) ) {
+            return;
+        }
+        window.fullViewServiceOrder.push(styleId);
+
+        if ( self.appendToElement.querySelector('style#' + styleId)) {
+            //next 
+            return;
+        }
+
+        var s = document.createElement("style");
+        s.setAttribute("id", styleId);
+        s.innerHTML = ""
+        s.innerHTML += "div#services-index button[aria-label=\"" + self.$translate.instant(service["title"]) + "\"] { order: " + order + " !important;}";
+        s.innerHTML += "div#services-index button[aria-label=\"" +service["title"] + "\"] { order: " + order + " !important;}";
+        s.innerHTML += "div.full-view-section#" + service["scrollId"] + " { order: " + order + " !important;}";
+
+        self.appendToElement.appendChild(s);
     }
 }
 
@@ -244,6 +147,7 @@ export let sectionOrderConfig = {
     name: 'custom-section-visibility-order',
     enabled: true,
     appendTo: 'prm-full-view-after',
+    enableInView: '.*',
     config: {
         bindings: {
             parentCtrl: '<'
