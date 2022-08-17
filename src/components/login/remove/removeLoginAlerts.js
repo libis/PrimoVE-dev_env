fe448d82746d67523b297ec637b5a9a60f56537e
Tom Vanmechelen <tom.vanmechelen@libis.be>
2022-08-17 15:11:27 +0200

    activate Login for DOSK

--- src/components/login/remove/removeLoginAlerts.js
+++ src/components/login/remove/removeLoginAlerts.js
@@ -30,7 +30,7 @@ export let RemoveLoginAlertsComponent = {
   },
   enabled: true,
   appendTo: ['prm-search-after','prm-request-services-after','alma-htgi-tabs-after'],
-  enableInView: '.*Lirias|32KUL_ACV:ACV.*|32KUL_BPB:BPB.*|32KUL_DOCVB:docvlaamsbrabant.*|32KUL_FARO:FARO.*|32KUL_FIN:FIN.*|32KUL_GSB.*|32KUL_GSG.*|32KUL_IMEC:IMEC.*|32KUL_KADOC:KADOC.*|32KUL_KBC:KBC.*|32KUL_NBB:NBB.*|32KUL_RBINS:RBINS.*|32KUL_RMCA:RMCA.*|32KUL_NBB:NBB.*|32KUL_TIFA:BOSA.*|32KUL_VLP:VLP.*|32KUL_VES:VDIC.*|.*JESUITS.*|.*DOKS.*'
+  enableInView: '.*Lirias|32KUL_ACV:ACV.*|32KUL_BPB:BPB.*|32KUL_DOCVB:docvlaamsbrabant.*|32KUL_FARO:FARO.*|32KUL_FIN:FIN.*|32KUL_GSB.*|32KUL_GSG.*|32KUL_IMEC:IMEC.*|32KUL_KADOC:KADOC.*|32KUL_KBC:KBC.*|32KUL_NBB:NBB.*|32KUL_RBINS:RBINS.*|32KUL_RMCA:RMCA.*|32KUL_NBB:NBB.*|32KUL_TIFA:BOSA.*|32KUL_VLP:VLP.*|32KUL_VES:VDIC.*|.*JESUITS.*'
 }
 
 
