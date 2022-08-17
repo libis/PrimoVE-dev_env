c4643b90c06e588c2764c5924661b51b606749a7
Tom Vanmechelen <tom.vanmechelen@libis.be>
2022-08-17 12:00:17 +0200

    News feed on homepage

--- src/components/newsFeed/index.js
+++ src/components/newsFeed/index.js
@@ -1,35 +1,72 @@
 import newsFeedTML from './newsFeed.html'
 
 class NewsFeedController {
-    constructor($scope,$ocLazyLoad,$injector) {
-        console.log('constructor');
+    constructor($scope,$http) {
+        var self = this;
+        self.scope = $scope;
         var locale = $scope.$root.$$childHead.$ctrl.userSessionManagerService.i18nService.getLanguage() ||  "en";
-        var dm = new Date();
-        dm.setHours(24, 0, 0, 0);
+        // console.log (locale)
+
         $scope.maxfeeds = 3;
-    
         $scope.feedresults = [];
-    /*
-        var filteredFeeds = feeds.filter(function (item) {
-            return item.feedLang.indexOf(locale) >= 0;
-        }); //feedLang equals locale language
-        //console.log (filteredFeeds)
-    
-        for (var f = 0; f <= 10 && f < filteredFeeds.length; f++) {
-            var feedConf = filteredFeeds[f];
-            // console.log(feedConf.feedUrl)
-            FeedService.parseFeed(feedConf).then(function (res) {
-                //    console.log('----FeedService.parseFeed(feedConf) ---------------------')
-                $scope.feedresults = $scope.feedresults.concat(res);
-    
-                FeedService.sortFeed($scope.feedresults);
-            });
-        }
-    */
+
+        let feeds = [
+            {
+                feedUrl: "https://limo-libis.blogspot.com/feeds/posts/default",
+                feedLang: ['en'],
+                feedContentType: 'title',
+                feedInst: "LIMO",
+                feedFilter: [
+                        { param: "entry.categories", value: "All Views" },
+                        { param: "entry.categories", value: "Odisee" },
+                        { param: "entry.categories", value: "KULeuven" },
+                        { param: "entry.categories", value: "KU Leuven Association" }
+                ]
+            },
+            {
+                feedUrl: "https://limo-libis-nl.blogspot.com/feeds/posts/default",
+                feedLang: ['nl'],
+                feedContentType: 'title',
+                feedInst: "LIMO",
+                feedFilter: [
+                        { param: "entry.categories", value: "All Views" },
+                        { param: "entry.categories", value: "Odisee" },
+                        { param: "entry.categories", value: "KULeuven" },
+                        { param: "entry.categories", value: "KU Leuven Association" }
+                ]
+            }
+        ];
+
+        var feedEndpoint = (feeds.filter(feed => feed['feedLang'] == locale ) )[0]['feedUrl']
+
+        feedEndpoint = feedEndpoint+"?alt=json-in-script&max-results=" + $scope.maxfeeds
+
+
+//        console.log (feedEndpoint)
+
+
+        $http.jsonp(feedEndpoint).then( function(res){
+            var entries = res.data.feed.entry
+              var daysBeforeToday = 30
+            var d1 = new Date(Date.now() - daysBeforeToday * 24 * 60 * 60 * 1000)
+
+            $scope.feedresults = entries.filter(entry => new Date(entry.updated.$t) > d1 )
+            $scope.feedresults = $scope.feedresults.map( entry => { 
+                // entry.updated.$t =  "today";  
+                var date = new Date(entry.updated.$t) 
+                const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
+                entry.updated.$t = day +"/"+ month +"/" + year;
+                entry.link = (entry.link.filter( link => link["rel"] == "alternate"))[0].href
+                return entry
+            })
+
+            // console.log ($scope.feedresults)
+          
+        });
     }
 }
 
-NewsFeedController.$inject = ['$scope','$ocLazyLoad','$injector']
+NewsFeedController.$inject = ['$scope','$http']
 
 export let newsFeedComponent = {
     name: 'custom-news-feed',
