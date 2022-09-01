1e3df03de3b1858165af4c246da7594594eab932
Tom Vanmechelen <tom.vanmechelen@libis.be>
2022-09-01 16:39:12 +0200

    pay fines message

---
+++ src/components/payFinesMessage/index.js
@@ -0,0 +1,37 @@
+class PayFinesMessageController {
+  constructor($translate, MessageService) {
+    let self = this;
+    Primo.user.then(user => {
+      self.user = user;
+      if (user.fines.length > 0) {
+        //TODO:extract html to its own file. find out how to resolve {{}}
+
+        let message = $translate.instant('nui.customization.fines.youHaveFines');
+        message = message.replace(/\$0/, user.fines.length);
+
+        let pay = $translate.instant('nui.customization.fines.pay');
+        
+        MessageService.show(`
+            <span style="align-self:center;">${message}</span>
+            <a style="background-color: tomato;color: white;"
+               class="md-button md-raised md-secundary" target='_blank'
+               href='https://services.libis.be/pay_my_fines'>${pay}</a>
+          `);
+      }
+    });
+  }
+}
+
+PayFinesMessageController.$inject = ['$translate', 'MessageService'];
+
+export let payFinesMessageComponent = {
+  name: 'custom-pay-fines-message',  
+  enabled: true,
+  appendTo: 'prm-top-bar-before',
+  enableInView: '.*',  
+  config: {
+    bindings: {parentCtrl: '<'},
+    controller: PayFinesMessageController,
+    template: ''
+  }
+}
