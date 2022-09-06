861c3a2fddc9da6d50cdb31559e60bc5152eecf0
Tom Vanmechelen <tom.vanmechelen@libis.be>
2022-09-06 13:28:46 +0200

    separation loginalert result list and full view

--- src/components/login/remove/removeLoginAlerts.js
+++ src/components/login/remove/removeLoginAlerts.js
@@ -12,7 +12,7 @@ class RemoveLoginAlertsController {
     // remove "Sign in" Alert in "How to get it"
     if( self.parentCtrl.reqAlert ){
       var s = document.createElement("style");
-      s.innerHTML = "alma-htgi-tabs > prm-alert-bar { display: none  !important }";
+      s.innerHTML = "prm-alert-bar { display: none  !important }";
       $element.append(angular.element(s));
     }
 
@@ -29,8 +29,8 @@ export let RemoveLoginAlertsComponent = {
     template: ''
   },
   enabled: true,
-  appendTo: ['prm-search-after','prm-request-services-after','alma-htgi-tabs-after'],
-  enableInView: '.*Lirias|32KUL_ACV:ACV.*|32KUL_BPB:BPB.*|32KUL_DOCVB:docvlaamsbrabant.*|32KUL_FARO:FARO.*|32KUL_FIN:FIN.*|32KUL_GSB.*|32KUL_GSG.*|32KUL_IMEC:IMEC.*|32KUL_KADOC:KADOC.*|32KUL_KBC:KBC.*|32KUL_NBB:NBB.*|32KUL_RBINS:RBINS.*|32KUL_RMCA:RMCA.*|32KUL_NBB:NBB.*|32KUL_TIFA:BOSA.*|32KUL_VLP:VLP.*|32KUL_VES:VDIC.*|.*JESUITS.*'
+  appendTo: ['prm-search-after'],
+  enableInView: '.*Lirias|32KUL_ACV:ACV.*|32KUL_BPB:BPB.*|32KUL_DOCVB:docvlaamsbrabant.*|32KUL_VCV:FARO.*|32KUL_FIN:FIN.*|32KUL_GSB.*|32KUL_GSG.*|32KUL_IMEC:IMEC.*|32KUL_KADOC:KADOC.*|32KUL_KBC:KBC.*|32KUL_NBB:NBB.*|32KUL_RBINS:RBINS.*|32KUL_RMCA:RMCA.*|32KUL_TIFA:BOSA.*|32KUL_VLP:VLP.*|32KUL_VES:VDIC.*|.*JESUITS.*|32KUL_KHL:UCLL'
 }
 
 
---
+++ src/components/login/remove/removeLoginAlertsFullView.js
@@ -0,0 +1,37 @@
+class RemoveLoginAlertsFullViewController {
+  constructor($scope,$element) {
+    let self = this;
+    // $ctrl.isLoggedInIDP()
+    self.parentCtrl = this.parentCtrl.parentCtrl
+
+    // remove "Sign in" Alert above resultlist
+    if(typeof  self.parentCtrl.isLoggedInIDP === "function"){
+      self.parentCtrl.isLoggedInIDP = function() { return true };
+    }
+  
+    // remove "Sign in" Alert in "How to get it"
+    if( self.parentCtrl.reqAlert ){
+      var s = document.createElement("style");
+      s.innerHTML = "alma-htgi-tabs > prm-alert-bar { display: none  !important }";
+      $element.append(angular.element(s));
+    }
+
+  }
+}
+
+RemoveLoginAlertsFullViewController.$inject = ['$scope','$element'];
+
+export let RemoveLoginAlertsFullViewComponent = {
+  name: 'custom-remove-login-alerts',
+  config: {
+    bindings: { parentCtrl: '<' },
+    controller: RemoveLoginAlertsFullViewController,
+    template: ''
+  },
+  enabled: true,
+  appendTo: ['prm-request-services-after','alma-htgi-tabs-after'],
+  enableInView: '.*Lirias|32KUL_ACV:ACV.*|32KUL_BPB:BPB.*|32KUL_DOCVB:docvlaamsbrabant.*|32KUL_VCV:FARO.*|32KUL_FIN:FIN.*|32KUL_GSB.*|32KUL_GSG.*|32KUL_IMEC:IMEC.*|32KUL_KADOC:KADOC.*|32KUL_KBC:KBC.*|32KUL_NBB:NBB.*|32KUL_RBINS:RBINS.*|32KUL_RMCA:RMCA.*|32KUL_TIFA:BOSA.*|32KUL_VLP:VLP.*|32KUL_VES:VDIC.*|.*JESUITS.*'
+}
+
+
+
