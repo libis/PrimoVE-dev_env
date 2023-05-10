007dd0430a7041d9e08230a724dc5720313dfaf2
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2023-05-10 15:07:28 +0200

    combine languages

--- src/components/sparkcentral/index.js
+++ src/components/sparkcentral/index.js
@@ -13,77 +13,41 @@ class SparkcentralController {
         Primo.view.then((view) => {
             let locale = view.interfaceLanguage;
 
-            if(locale == "nl"){
-                Sparkcentral.init({
-                    appId: '5e2af1872e81a50010fa5f95',
-                    region: 'eu-1',
-                    locale: 'nl-BE',
-                    soundNotificationEnabled: true,
-                    customColors: {
-                        brandColor: '328eb0',
-                        conversationColor: '328eb0',
-                        actionColor: '65beec',
-                    },
-                    customText: {
-                        conversationTimestampHeaderFormat: 'DD MMMM YYYY, H:mm',
-                        introductionText: 'Chat met een medewerker van KU Leuven Bibliotheken op werkdagen tussen 9:00 en 16:00.',
-                        headerText: 'Heb je een vraag?',               
-                        inputPlaceholder: 'Typ hier je boodschap',
-                        messageSending: 'Verzenden...',
-                        sendButtonText: 'Verzenden',
-                        uploadPhoto: 'Upload een foto',
-                        shareLocation: 'Deel je locatie',
-                        uploadDocument: 'Upload een document',
-                    },
-                    buttonIconUrl: 'https://admin.kuleuven.be/icts/servicepunt/images/chaticoontje.gif',
-                    businessName: 'KU Leuven Bibliotheken',
-                    businessIconUrl: 'https://bib.kuleuven.be/rbib/afbeeldingen/chat-logo-bib.png',
-                    backgroundImageUrl: 'http://stijl.kuleuven.be/2016/img/sedes-kuleuven.png',
-                    menuItems: {
-                        imageUpload: 'Yes',
-                        fileUpload: 'Yes',
-                        shareLocation: 'Yes'
-                    }
-                })
-                .then(function() {
-                    // Your code after init is complete
-                });
-            }else{
-                Sparkcentral.init({
-                    appId: '5e2af1872e81a50010fa5f95',
-                    region: 'eu-1',
-                    locale: 'nl-BE',
-                    soundNotificationEnabled: true,
-                    customColors: {
-                        brandColor: '328eb0',
-                        conversationColor: '328eb0',
-                        actionColor: '65beec',
-                    },
-                    customText: {
-                        conversationTimestampHeaderFormat: 'DD MMMM YYYY, H:mm',
-                        introductionText: 'Chat with a staff member of KU Leuven Libraries on working days between 9 AM and 4 PM.',                
-                        headerText: 'Do you have a question?',
-                        inputPlaceholder: 'Please type your message',
-                        messageSending: 'Sending...',
-                        sendButtonText: 'Send',
-                        uploadPhoto: 'Upload a picture',
-                        shareLocation: 'Share your location',
-                        uploadDocument: 'Upload a document',
-                    },
-                    buttonIconUrl: 'https://admin.kuleuven.be/icts/servicepunt/images/chaticoontje.gif',
-                    businessName: 'KU Leuven Libraries',
-                    businessIconUrl: 'https://bib.kuleuven.be/rbib/afbeeldingen/chat-logo-bib.png',
-                    backgroundImageUrl: 'http://stijl.kuleuven.be/2016/img/sedes-kuleuven.png',
-                    menuItems: {
-                        imageUpload: 'Yes',
-                        fileUpload: 'Yes',
-                        shareLocation: 'Yes'
-                    }
-                })
-                .then(function() {
-                    // Your code after init is complete
-                });
-            }
+            Sparkcentral.init({
+                appId: '5e2af1872e81a50010fa5f95',
+                region: 'eu-1',
+                locale: 'nl-BE',
+                soundNotificationEnabled: true,
+                customColors: {
+                    brandColor: '328eb0',
+                    conversationColor: '328eb0',
+                    actionColor: '65beec',
+                },
+                customText: {
+                    conversationTimestampHeaderFormat: 'DD MMMM YYYY, H:mm',
+                    introductionText: 'Chat met een medewerker van KU Leuven Bibliotheken op werkdagen tussen 9:00 en 16:00. / Chat with a staff member of KU Leuven Libraries on working days between 9 AM and 4 PM.',
+                    headerText: 'Heb je een vraag? / Do you have a question?',               
+                    inputPlaceholder: 'Typ hier je boodschap / Please type your message',
+                    messageSending: 'Verzenden... / Sending...',
+                    sendButtonText: 'Verzenden / Send',
+                    uploadPhoto: 'Upload een foto / Upload a picture',
+                    shareLocation: 'Deel je locatie / Share your location',
+                    uploadDocument: 'Upload een document / Upload a document',
+                },
+                buttonIconUrl: 'https://admin.kuleuven.be/icts/servicepunt/images/chaticoontje.gif',
+                businessName: 'KU Leuven Bibliotheken',
+                businessIconUrl: 'https://bib.kuleuven.be/rbib/afbeeldingen/chat-logo-bib.png',
+                backgroundImageUrl: 'http://stijl.kuleuven.be/2016/img/sedes-kuleuven.png',
+                menuItems: {
+                    imageUpload: 'Yes',
+                    fileUpload: 'Yes',
+                    shareLocation: 'Yes'
+                }
+            })
+            .then(function() {
+                // Your code after init is complete
+            });
+            
         });      
        
     }
