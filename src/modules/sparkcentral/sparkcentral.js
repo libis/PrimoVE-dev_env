4c82fd6d3c32e9eb3ca403679010db38c9963099
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2023-06-07 14:30:44 +0200

    change sparkcentral loading

--- src/modules/sparkcentral/sparkcentral.js
+++ src/modules/sparkcentral/sparkcentral.js
@@ -1,5 +1,5 @@
-angular.module('sparkcentral', ['angularLoad']).run(['$translate','$rootScope',  function($translate,$rootScope){
-  var loaderUrl = 'https://cdn.sparkcentral.com/rtm/web/1.17.0/loader.json'
-  !function(e,n,t,r){function s(){try{var e;if((e="string"==typeof this.response?JSON.parse(this.response):this.response).url){var t=n.getElementsByTagName("script")[0],r=n.createElement("script");r.async=!0,r.src=e.url,t.parentNode.insertBefore(r,t)}}catch(e){}}var o,p,a,c=[],i=[];e[t]={init:function(){o=arguments;var e={then:function(n){return i.push({type:"t",next:n}),e},catch:function(n){return i.push({type:"c",next:n}),e}};return e},on:function(){c.push(arguments)},render:function(){p=arguments},destroy:function(){a=arguments}},e.__onWebMessengerHostReady__=function(n){if(delete e.__onWebMessengerHostReady__,e[t]=n,o)for(var r=n.init.apply(n,o),s=0;s<i.length;s++){var u=i[s];r="t"===u.type?r.then(u.next):r.catch(u.next)}p&&n.render.apply(n,p),a&&n.destroy.apply(n,a);for(s=0;s<c.length;s++)n.on.apply(n,c[s])};var u=new XMLHttpRequest;u.addEventListener("load",s),u.open("GET",loaderUrl,!0),u.responseType="json",u.send()}(window,document,"Sparkcentral");
-    
+angular.module('sparkcentral', ['angularLoad']).run(['angularLoad',(angularLoad) => {
+  angularLoad.loadScript('https://cdn.sparkcentral.com/rtm/web/1.17.0/sparkcentral.1.17.0.min.js').then(function () {
+      console.log('sparkcentral loaded');
+  });  
 }]);
