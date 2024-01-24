6ea8cc8a35bcf623fa033af0acf991d05209e24f
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2024-01-24 10:40:18 +0100

    fix payFinesMessage

--- src/components/payFinesMessage/index.js
+++ src/components/payFinesMessage/index.js
@@ -4,22 +4,26 @@ class PayFinesMessageController {
   constructor($translate, MessageService) {
     let self = this;
     self.user = Session.user;    
+   
+    self.user.fines.then(fines => {
+      self.fines = fines;
+      if (fines.length > 0) {
+        //TODO:extract html to its own file. find out how to resolve {{}}
+  
+        let message = $translate.instant('nui.customization.fines.youHaveFines');
+        message = message.replace(/\$0/, fines.length);
     
-    if (self.user.fines.length > 0) {
-      //TODO:extract html to its own file. find out how to resolve {{}}
-
-      let message = $translate.instant('nui.customization.fines.youHaveFines');
-      message = message.replace(/\$0/, user.fines.length);
-
-      let pay = $translate.instant('nui.customization.fines.pay');
-      
-      MessageService.show(`
-          <span style="align-self:center;">${message}</span>
-          <a style="background-color: tomato;color: white;"
-              class="md-button md-raised md-secundary" target='_blank'
-              href='https://services.libis.be/pay_my_fines'>${pay}</a>
-        `);
-    }
+        let pay = $translate.instant('nui.customization.fines.pay');
+        
+        MessageService.show(`
+            <span style="align-self:center;">${message}</span>
+            <a style="background-color: tomato;color: white;"
+                class="md-button md-raised md-secundary" target='_blank'
+                href='https://services.libis.be/pay_my_fines'>${pay}</a>
+          `);
+      }
+    });
+    
   }
 }
 
