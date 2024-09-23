04aaf554202f959bafd5b333e8a874378a8e350e
Lydia Janssen <lydia.janssen@kuleuven.be>
2024-09-23 17:28:46 +0200

    RA feedback interceptor - initial

--- src/modules/pubSubInterceptor/interceptors/6_captureFeedback/rafeedback.js
+++ src/modules/pubSubInterceptor/interceptors/6_captureFeedback/rafeedback.js
@@ -1,27 +1,7 @@
-pubSub.subscribe('after-raFeedbackURL', (reqRes) => {
+pubSub.subscribe('before-raFeedbackURL', (reqRes) => {
     console.log('Arrived at after-raFeedbackURL');
     console.log('feedback message', reqRes);
-    //if (Object.keys(reqRes.config.params).includes('blendFacetsSeparately')) {
-    //    reqRes.data.docs.map((m) => {
-    //        try {
-    //            if (m.adaptor === 'SearchWebhook') {                    
-    //                m.delivery.link = m.delivery.link == null ? [] : m.delivery.link;
-    //                m.pnx.links.thumbnail.forEach((url, i) => {
-    //                    if (/^\$\$U/.test(url)) {
-    //                        url = url.replace(/^\$\$U/, '')
-    //                        m.delivery.link.push({
-    //                            "@id": `_:${i}`,
-    //                            "linkType": "http://purl.org/pnx/linkType/thumbnail",
-    //                            "linkURL": url,
-    //                            "displayLabel": "thumbnail"
-    //                        });
-    //                    }
-    //                });
-    //            }
-    //        } catch (error) {
-    //            console.error(`No thumbnail for record:${m.pnx.control.recordid}`);
-    //        }
-    //    });        
-    //}
+
+
     return reqRes
 });
