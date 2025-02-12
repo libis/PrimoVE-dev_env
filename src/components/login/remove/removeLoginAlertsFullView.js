90ecf67d422419a31333522821df7d50f023f07c
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2025-02-12 16:37:11 +0100

    remove login from sportimonium

--- src/components/login/remove/removeLoginAlertsFullView.js
+++ src/components/login/remove/removeLoginAlertsFullView.js
@@ -34,7 +34,7 @@ export let RemoveLoginAlertsFullViewComponent = {
   },
   enabled: true,
   appendTo: ['prm-request-services-after','alma-htgi-tabs-after','prm-alma-viewit-after'],
-  enableInView: '.*Lirias|32KUL_ACV:ACV.*|32KUL_BPB:BPB.*|32KUL_DOCVB:docvlaamsbrabant.*|32KUL_VCV:FARO.*|32KUL_FIN:FODFIN.*|32KUL_GSB.*|32KUL_GSG.*|32KUL_IMEC:IMEC.*|32KUL_KADOC:KADOC.*|32KUL_KBC:KBC.*|32KUL_KMKG:KMKG.*|32KUL_NBB:NBB.*|32KUL_RBINS:RBINS.*|32KUL_RMCA:RMCA.*|32KUL_TIFA:BOSA.*|32KUL_VLP:VLP.*|32KUL_VES:VDIC.*|.*JESUITS.*'
+  enableInView: '.*Lirias|32KUL_ACV:ACV.*|32KUL_BPB:BPB.*|32KUL_DOCVB:docvlaamsbrabant.*|32KUL_VCV:FARO.*|32KUL_FIN:FODFIN.*|32KUL_GSB.*|32KUL_GSG.*|32KUL_IMEC:IMEC.*|32KUL_KADOC:KADOC.*|32KUL_KBC:KBC.*|32KUL_KMKG:KMKG.*|32KUL_NBB:NBB.*|32KUL_RBINS:RBINS.*|32KUL_RMCA:RMCA.*|32KUL_TIFA:BOSA.*|32KUL_VLP:VLP.*|32KUL_VES:VDIC.*|.*JESUITS.*|32KUL_KUL:sportimonium'
 }
 
 
