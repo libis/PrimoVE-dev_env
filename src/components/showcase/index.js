3e06ccbcc3d0ab7f890491c9947ed715682f9249
Joris Lambrechts <joris.lambrechts@kuleuven.be>
2025-03-26 10:39:42 +0100

    add watcher

--- src/components/showcase/index.js
+++ src/components/showcase/index.js
@@ -13,28 +13,43 @@ class ShowcaseController {
         let $scope = self.$scope;
         self.element = angular.element(document.querySelector("prm-showcase md-content md-card#Showcase md-card-content"));
 
-        console.log(self.element);
+        let watcher = self.$scope.$watch(() => {
+            try {
+              if (self.$translate.instant('nui.customization.showcase.url') == 'nui.customization.showcase.url') {
+                return false;
+              } else {
+                return true;
+              }
+            } catch (e) {
+              return false;
+            }
+        }, (n, o) => {
+            if (n == true) {        
+                 //url comes from the code table
+                $scope.showcaseUrl = decodeURIComponent( self.$translate.instant( "nui.customization.showcase.url" ));
 
-        //url comes from the code table
-        $scope.showcaseUrl = decodeURIComponent( self.$translate.instant( "nui.customization.showcase.url" ));
+                var month = '0' + (new Date().getMonth() + 1).toString().slice(-2);
+                var prev_month = '0' + (new Date().getMonth()).toString().slice(-2);
+            
+                var prev_date = new Date();
+                prev_date.setMonth(prev_date.getMonth()-1);
+                var prev_year= prev_date.getFullYear().toString().slice(-2);
+                var year= new Date().getFullYear().toString().slice(-2);
+            
+                //might need changing for other with other views
+                $scope.showcaseUrl = $scope.showcaseUrl.replace("2502", year + month);
+                $scope.showcaseUrl = $scope.showcaseUrl.replace("2501", prev_year + prev_month);
 
-        var month = '0' + (new Date().getMonth() + 1).toString().slice(-2);
-        var prev_month = '0' + (new Date().getMonth()).toString().slice(-2);
+                $scope.titleText = decodeURIComponent( self.$translate.instant( "nui.customization.showcase.titleText" ));
+                
+                //$scope.titleText = "Showcase"
+                $scope.showcase_tmp = '<search-carousel titleText="'+ $scope.titleText +'" searchUrl='+ $scope.showcaseUrl +'></search-carousel>';
+                self.element.append( $scope.showcase_tmp  );
+                watcher();
+            }
+        })
+
        
-        var prev_date = new Date();
-        prev_date.setMonth(prev_date.getMonth()-1);
-        var prev_year= prev_date.getFullYear().toString().slice(-2);
-        var year= new Date().getFullYear().toString().slice(-2);
-       
-        //might need changing for other with other views
-        $scope.showcaseUrl = $scope.showcaseUrl.replace("2502", year + month);
-        $scope.showcaseUrl = $scope.showcaseUrl.replace("2501", prev_year + prev_month);
-
-        $scope.titleText = decodeURIComponent( self.$translate.instant( "nui.customization.showcase.titleText" ));
-        
-        //$scope.titleText = "Showcase"
-        $scope.showcase_tmp = '<search-carousel titleText="'+ $scope.titleText +'" searchUrl='+ $scope.showcaseUrl +'></search-carousel>';
-        self.element.append( $scope.showcase_tmp  );
     }
 }
 
