2247ffaed5eb3d19e2d0181836b7c911c7fdb55f
unknown <semmi.ezaitouni@kuleuven.be>
2025-11-05 14:33:35 +0100

    login only visible for curretn view LIBS fixed

--- src/components/login/autoLogin_first_option/index.js
+++ src/components/login/autoLogin_first_option/index.js
@@ -1,36 +1,62 @@
-
 class AutoLoginFirstOptionController {
-  constructor($scope) { 
+  constructor($scope) {
     this.$scope = $scope;
   }
 
   $onInit() {
     var self = this;
     self.parentCtrl = this.parentCtrl.parentCtrl;
-    /*
-    console.log ( "AutoLoginFirstOptionController" )
-    console.log ( self )
-    console.log ( self.parentCtrl.authenticationMethods )
-    console.log ( self.parentCtrl.authenticationMethods[0] )    
-    */
-   
-    self.parentCtrl.handleLoginClick( self.parentCtrl.authenticationMethods[0] )      
 
-    
-    
+    console.log("autoLoginFirstOptionController initialized");
+
+    const vid = window.appConfig.vid;
+    console.log(" current vid", vid);
+
+    const allowedVids = ["32KUL_LIBS:LIBS", "32KUL_LIBS:RVAONEM"];
+
+    const profileMap = {
+      "32KUL_LIBS:LIBS": "Alma",
+      "32KUL_LIBS:RVAONEM": "RVA_AZURE",
+    };
+    const targetProfile = profileMap[vid];
+    console.log(" target profile is: " + targetProfile);
+
+    if (allowedVids.includes(vid) && self.parentCtrl.authenticationMethods) {
+      const filteredMethods = self.parentCtrl.authenticationMethods.filter(
+        (method) => {
+          console.log("checking method.profileName: " + method.profileName);
+          return method.profileName === targetProfile;
+        }
+      );
+
+      self.parentCtrl.authenticationMethods = filteredMethods;
+
+      if (filteredMethods.length > 0) {
+        console.log(
+          "autologin triggered with: " + filteredMethods[0].profileName
+        );
+        self.parentCtrl.handleLoginClick(filteredMethods[0]);
+      } else {
+        console.log(
+          "no matching authmethod found for profile: " + targetProfile
+        );
+      }
+    } else {
+      console.log("this is mot working");
+    }
   }
 }
 
-AutoLoginFirstOptionController.$inject = ['$scope'];
+AutoLoginFirstOptionController.$inject = ["$scope"];
 
 export let AutoLoginFirstOptionComponent = {
-  name: 'custom-automatic-login-first-option',
+  name: "custom-automatic-login-first-option",
   config: {
-    bindings: { parentCtrl: '<' },
+    bindings: { parentCtrl: "<" },
     controller: AutoLoginFirstOptionController,
-    template: ''
+    template: "",
   },
   enabled: true,
-  appendTo: 'prm-login-after',
-  enableInView: '^32KUL_KUL:(?!KULeuven_ra|KULeuven_TEST)'
-}
+  appendTo: "prm-login-after",
+  enableInView: "^32KUL_LIBS:.*",
+};
