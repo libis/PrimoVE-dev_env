60d533f264dc08e95bc80f586159921e337acc38
unknown <semmi.ezaitouni@kuleuven.be>
2026-04-27 15:55:40 +0200

    commit for browzine error

--- resources/32KUL_LIBIS_NETWORK-CENTRAL_PACKAGE/js/custom.js
+++ resources/32KUL_LIBIS_NETWORK-CENTRAL_PACKAGE/js/custom.js
@@ -1,11 +1,11 @@
-(function(){
-"use strict";
-'use strict';
+(function () {
+  "use strict";
+  "use strict";
 
-var app = angular.module('viewCustom', ['angularLoad']);
-window.browzine = {
-    api: "https://public-api.thirdiron.com/public/v1/libraries/1624",
-    apiKey: "e71d1c31-7938-470a-8be2-a6e351e0c001",
+  var app = angular.module("viewCustom", ["angularLoad"]);
+  window.browzine = {
+    api: "https://public-api.thirdiron.com/public/v1/libraries/1781",
+    apiKey: "0d9d0987-0cd6-4df4-85f5-c78933156554",
     journalBrowZineWebLinkText: "View Journal Contents",
     articleBrowZineWebLinkText: "View Issue Contents",
     articlePDFDownloadLinkEnabled: true,
@@ -14,117 +14,139 @@ window.browzine = {
     articleLinkText: "Read Article",
     unpaywallEmailAddressKey: "helpdesk@libisnet.be",
     articlePDFDownloadViaUnpaywallEnabled: true,
-    articlePDFDownloadViaUnpaywallText: "Download PDF (via Unpaywall)"
-};
+    articlePDFDownloadViaUnpaywallText: "Download PDF (via Unpaywall)",
+  };
 
-browzine.script = document.createElement("script");
-browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
-document.head.appendChild(browzine.script);
+  browzine.script = document.createElement("script");
+  browzine.script.src =
+    "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
+  document.head.appendChild(browzine.script);
 
-app.controller('prmSearchResultAvailabilityLineAfterController', function ($scope) {
-    window.browzine.primo.searchResult($scope);
-});
-
-app.component('prmSearchResultAvailabilityLineAfter', {
-    bindings: { parentCtrl: '<' },
-    controller: 'prmSearchResultAvailabilityLineAfterController'
-});
-
-app.component('prmServiceDetailsAfter', {
-    bindings: {
-        parentCtrl: '<'
+  app.controller(
+    "prmSearchResultAvailabilityLineAfterController",
+    function ($scope) {
+      window.browzine.primo.searchResult($scope);
     },
-    controller: 'prmServiceDetailsAfter',
-    templateUrl: ''
-});
+  );
 
-app.controller('prmServiceDetailsAfter', ['$scope', '$compile', function ($scope, $compile) {
-    self = this;
-    this.$scope = $scope;
-    this.$compile = $compile;
+  app.component("prmSearchResultAvailabilityLineAfter", {
+    bindings: { parentCtrl: "<" },
+    controller: "prmSearchResultAvailabilityLineAfterController",
+  });
 
-    self.removeDescriptionIfDb = function () {
+  app.component("prmServiceDetailsAfter", {
+    bindings: {
+      parentCtrl: "<",
+    },
+    controller: "prmServiceDetailsAfter",
+    templateUrl: "",
+  });
 
+  app.controller("prmServiceDetailsAfter", [
+    "$scope",
+    "$compile",
+    function ($scope, $compile) {
+      self = this;
+      this.$scope = $scope;
+      this.$compile = $compile;
+
+      self.removeDescriptionIfDb = function () {
         if (self.parentCtrl.item.pnx.display.type.includes("database")) {
-
-            for (var d in self.parentCtrl.details) {
-                if (self.parentCtrl.details[d].label === "description") {
-                    self.parentCtrl.details[d].values[0].values = '';
-                }
+          for (var d in self.parentCtrl.details) {
+            if (self.parentCtrl.details[d].label === "description") {
+              self.parentCtrl.details[d].values[0].values = "";
             }
+          }
         }
-    };
+      };
 
-    self.waitForPNX = function () {
+      self.waitForPNX = function () {
         var _this = this;
 
         console.log(this);
-        var unbindWatcher = self.$scope.$watch(function () {
+        var unbindWatcher = self.$scope.$watch(
+          function () {
             return self.parentCtrl.details;
-        }, function (newVal, oldVal) {
+          },
+          function (newVal, oldVal) {
             if (newVal) {
-                unbindWatcher();
-                _this.removeDescriptionIfDb();
+              unbindWatcher();
+              _this.removeDescriptionIfDb();
             }
-        });
-    };
+          },
+        );
+      };
 
-    self.waitForPNX();
-}]);
-
-/* https://otago.hosted.exlibrisgroup.com/primo-explore/search?vid=DUNEDIN&sortby=rank&search_scope=All */
-
-/* document.title => See Code Table Header/Footer Tiles */
-document.querySelectorAll("link[rel='icon']")[0].setAttribute("href", "/discovery/custom/49ECB_INST-ECB/img/favicon.png");
-
-/* TODO : get this on Central Package level */
-
-app.component('prmLinkedUserSelectorAfter', {
-    bindings: {
-        parentCtrl: '<'
+      self.waitForPNX();
     },
-    controller: 'prmLinkedUserSelectorAfter',
-    templateUrl: '/discovery/custom/49ECB_INST-ECB/html/prmLinkedUserSelectorAfter.html'
-});
+  ]);
 
-app.controller('prmLinkedUserSelectorAfter', ['$element', '$scope', '$location', '$templateCache', function ($element, $scope, $location, $templateCach) {
-    var ctrl = this;
-    var scope = $scope;
-    var vid = scope.$parent.$parent.$ctrl.vid;
+  /* https://otago.hosted.exlibrisgroup.com/primo-explore/search?vid=DUNEDIN&sortby=rank&search_scope=All */
 
-    $scope.signOut = function () {
+  /* document.title => See Code Table Header/Footer Tiles */
+  document
+    .querySelectorAll("link[rel='icon']")[0]
+    .setAttribute("href", "/discovery/custom/49ECB_INST-ECB/img/favicon.png");
+
+  /* TODO : get this on Central Package level */
+
+  app.component("prmLinkedUserSelectorAfter", {
+    bindings: {
+      parentCtrl: "<",
+    },
+    controller: "prmLinkedUserSelectorAfter",
+    templateUrl:
+      "/discovery/custom/49ECB_INST-ECB/html/prmLinkedUserSelectorAfter.html",
+  });
+
+  app.controller("prmLinkedUserSelectorAfter", [
+    "$element",
+    "$scope",
+    "$location",
+    "$templateCache",
+    function ($element, $scope, $location, $templateCach) {
+      var ctrl = this;
+      var scope = $scope;
+      var vid = scope.$parent.$parent.$ctrl.vid;
+
+      $scope.signOut = function () {
         scope.$root.$$childHead.$ctrl.userSessionManagerService.userLogout();
         //  document.location.href = "https://leuven-primo.hosted.exlibrisgroup.com/pds?func=logout&calling_system=primo&institute=ECB&url=" + document.location.origin + "/primo-explore/search?vid="+ vid +"%26performLogout=true"
-    };
-}]);
-
-app.component('prmUserAreaAfter', {
-    bindings: {
-        parentCtrl: '<'
+      };
     },
-    controller: 'prmUserAreaAfterController',
-    templateUrl: '/discovery/custom/49ECB_INST-ECB/html/user-area.html'
-});
+  ]);
 
-app.controller('prmUserAreaAfterController', ['$element', '$scope', '$location', '$templateCache', function ($element, $scope, $location, $templateCache) {
+  app.component("prmUserAreaAfter", {
+    bindings: {
+      parentCtrl: "<",
+    },
+    controller: "prmUserAreaAfterController",
+    templateUrl: "/discovery/custom/49ECB_INST-ECB/html/user-area.html",
+  });
 
-    var ctrl = this;
-    ctrl.$onInit = function () {
+  app.controller("prmUserAreaAfterController", [
+    "$element",
+    "$scope",
+    "$location",
+    "$templateCache",
+    function ($element, $scope, $location, $templateCache) {
+      var ctrl = this;
+      ctrl.$onInit = function () {
         ctrl.$element = $element;
-    };
+      };
 
-    ctrl.$postLink = function () {
+      ctrl.$postLink = function () {
         ctrl.insertUserSignin();
-    };
+      };
 
-    ctrl.insertUserSignin = function () {
-        var elementRemove = document.getElementsByTagName('prm-user-area')[0];
+      ctrl.insertUserSignin = function () {
+        var elementRemove = document.getElementsByTagName("prm-user-area")[0];
         var i = elementRemove.children.length - 1;
         while (i >= 0) {
-            if (elementRemove.children[i].tagName !== 'PRM-USER-AREA-AFTER') {
-                angular.element(elementRemove.children[i]).remove();
-            }
-            i--;
+          if (elementRemove.children[i].tagName !== "PRM-USER-AREA-AFTER") {
+            angular.element(elementRemove.children[i]).remove();
+          }
+          i--;
         }
         /*
         console.log (elementRemove) 
@@ -133,25 +155,24 @@ app.controller('prmUserAreaAfterController', ['$element', '$scope', '$location',
         angular.element(elementRemove.children[0]).remove();
         */
         $scope.signIn = function () {
-            ctrl.parentCtrl.loginIframeService.loginService.handleLoginClick();
+          ctrl.parentCtrl.loginIframeService.loginService.handleLoginClick();
         };
-    };
-}]);
-
-app.component('prmVirtualBrowseItemAfter', {
-    bindings: {
-        parentCtrl: '<'
+      };
     },
-    controller: 'prmVirtualBrowseItemAfter',
-    templateUrl: ''
-});
+  ]);
 
-app.controller('prmVirtualBrowseItemAfter', ['$element', function ($element) {
-    $element.parent().parent().attr("target", "_blank");
-}]);
+  app.component("prmVirtualBrowseItemAfter", {
+    bindings: {
+      parentCtrl: "<",
+    },
+    controller: "prmVirtualBrowseItemAfter",
+    templateUrl: "",
+  });
+
+  app.controller("prmVirtualBrowseItemAfter", [
+    "$element",
+    function ($element) {
+      $element.parent().parent().attr("target", "_blank");
+    },
+  ]);
 })();
-
-
-
-
-
