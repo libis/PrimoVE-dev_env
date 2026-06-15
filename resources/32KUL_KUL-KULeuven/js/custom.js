e4e72a10848cb7330671193299de60e13ce41aad
unknown <semmi.ezaitouni@kuleuven.be>
2026-06-15 15:09:55 +0200

    bug sportimonium news fixed

--- resources/32KUL_KUL-KULeuven/js/custom.js
+++ resources/32KUL_KUL-KULeuven/js/custom.js
@@ -1,15 +1,10 @@
-
-
 import "../css/index.css";
+import { payFinesMessageComponent } from "../../../src/components/payFinesMessage/index";
 
-(function(){
-"use strict";
+(function () {
+  "use strict";
 
-var app = angular.module('viewCustom', ['angularLoad']);
+  var app = angular.module("viewCustom", ["angularLoad"]);
 
+  app.component(payFinesMessageComponent.name, payFinesMessageComponent.config);
 })();
-
-
-
-
-
--- src/components/payFinesMessage/index.js
+++ src/components/payFinesMessage/index.js
@@ -1,42 +1,52 @@
-import Session from '../../primo/session'
+import Session from "../../primo/session";
 
 class PayFinesMessageController {
   constructor($translate, MessageService) {
     let self = this;
-    self.user = Session.user;    
+    self.user = Session.user;
 
-      self.user.fines.then(fines => {
-          self.fines = fines.filter((fine) => fine.finestatus == 'ACTIVE');
+    self.user.fines.then((fines) => {
+      self.fines = fines.filter((fine) => fine.finestatus == "ACTIVE");
 
-          if (self.fines.length > 0) {
-              //TODO:extract html to its own file. find out how to resolve {{}}
+      if (self.fines.length > 0) {
+        //TODO:extract html to its own file. find out how to resolve {{}}
 
-              let message = $translate.instant('nui.customization.fines.youHaveFines');
-              message = message.replace(/\$0/, self.fines.length);
+        let message = $translate.instant(
+          "nui.customization.fines.youHaveFines",
+        );
+        message = message.replace(/\$0/, self.fines.length);
 
-              let pay = $translate.instant('nui.customization.fines.pay');
-              
-              MessageService.show(`
-          <span style="align-self:center;">${message}</span>
-          <a style="background-color: tomato;color: white;"
-              class="md-button md-raised md-secundary" target='_blank'
-              href='https://services.libis.be/pay_my_fines'>${pay}</a>
-        `);
-          }
-      });
+        let pay = $translate.instant("nui.customization.fines.pay");
+
+        //       MessageService.show(`
+        //   <span style="align-self:center;">${message}</span>
+        //   <a style="background-color: tomato;color: white;"
+        //       class="md-button md-raised md-secundary" target='_blank'
+        //       href='https://services.libis.be/pay_my_fines'>${pay}</a>
+        // `);
+        console.log("this is an alien speaking");
+        MessageService.show(`
+  <span style="align-self:center;">${message}</span>
+  <a style="background-color: tomato;color: white;"
+      class="md-button md-raised md-secundary"
+      href='javascript:void(0)'
+      onclick="alert('user clicked pay')">PAY NOW</a>
+`);
+      }
+    });
   }
 }
 
-PayFinesMessageController.$inject = ['$translate', 'MessageService'];
+PayFinesMessageController.$inject = ["$translate", "MessageService"];
 
 export let payFinesMessageComponent = {
-  name: 'custom-pay-fines-message',  
+  name: "custom-pay-fines-message",
   enabled: true,
-  appendTo: 'prm-top-bar-before',
-  enableInView: '32KUL_KUL:.*',  
+  appendTo: "prm-top-bar-before",
+  enableInView: "32KUL_KUL:.*",
   config: {
-    bindings: {parentCtrl: '<'},
+    bindings: { parentCtrl: "<" },
     controller: PayFinesMessageController,
-    template: ''
-  }
-}
+    template: "",
+  },
+};
